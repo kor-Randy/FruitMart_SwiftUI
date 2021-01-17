3bfa504ac58f527000ecaff60535b5ad863d7b18
kor-Randy <randy_95@naver.com>
2021-01-17 20:13:21 +0900

    Plus ObservableObject on Store for Use @EnvironmentObject

--- FruitMart/Source/Model/Store.swift
+++ FruitMart/Source/Model/Store.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-final class Store:  {
-    var products: [Product]
+final class Store: ObservableObject {
+    //특정 상품의 데이터가 변경되면, 된련된 뷰들이 모두 알아채고 화면을 올바르게 갱신하기 위함
+    @Published var products: [Product]
 
     init(filename: String = "ProductData.json") {
         self.products = Bundle.main.decode(filename: filename, as: [Product].self)
